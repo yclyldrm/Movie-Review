21c06cb84ea00e7a20fe707f0c79c2755bdfc724
yclyldrm <yclyldrm@bitbucket.org>
2018-08-13 17:44:35 +0300

    Add review in movie page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,7 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  before_action :set_movie
 
   def index
     @reviews = Review.all
@@ -18,11 +20,12 @@ class ReviewsController < ApplicationController
 
   def create
     @review = Review.new(review_params)
-    @review.user.id = current_user.id
+    @review.user_id = current_user.id
+    @review.movie_id = @movie.id
     respond_to do |format|
       if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
-        format.json { render :show, status: :created, location: @review }
+        format.html { redirect_to @movie, notice: 'Review was successfully created.' }
+        format.json { render :show, status: :created, location: @movie }
       else
         format.html { render :new }
         format.json { render json: @review.errors, status: :unprocessable_entity }
@@ -53,6 +56,10 @@ class ReviewsController < ApplicationController
   private
     def set_review
       @review = Review.find(params[:id])
+    end
+
+    def set_movie
+      @movie = Movie.find(params[:movie_id])
     end
 
     def review_params
